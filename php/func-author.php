fc74f55f1fe57c661cea68bcf4f5f1acb84eed13
ToskaK6 <toskakastrati@gmail.com>
2025-01-27 19:20:27 +0100

    Create func-author.php

---
+++ php/func-author.php
@@ -0,0 +1,29 @@
+<?php 
+
+function get_all_author($con){
+   $sql  = "SELECT * FROM authors";
+   $stmt = $con->prepare($sql);
+   $stmt->execute();
+
+   if ($stmt->rowCount() > 0) {
+   	  $authors = $stmt->fetchAll();
+   }else {
+      $authors = 0;
+   }
+
+   return $authors;
+}
+
+function get_author($con, $id){
+   $sql  = "SELECT * FROM authors WHERE id=?";
+   $stmt = $con->prepare($sql);
+   $stmt->execute([$id]);
+
+   if ($stmt->rowCount() > 0) {
+   	  $author = $stmt->fetch();
+   }else {
+      $author = 0;
+   }
+
+   return $author;
+}
