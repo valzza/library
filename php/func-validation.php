f9361049347985e0163ab4032065ff6f37245b5b
ToskaK6 <toskakastrati@gmail.com>
2025-01-27 19:35:23 +0100

    Create func-validation.php

---
+++ php/func-validation.php
@@ -0,0 +1,13 @@
+<?php
+
+function is_empty($var, $text, $location, $ms, $data){
+    if(empty($var)) {
+        $em = "The ".$text." is required";
+        header("Location: $location?$ms=$em&$data");
+        exit;
+    }
+    return 0;
+}
+
+
+?>
