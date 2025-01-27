2929aaece1c029ebf5575a56f837624125b18506
jetamurtezi <jetamurtezi98@gmail.com>
2025-01-27 19:26:45 +0100

    func-category.php

---
+++ php/func-category.php
@@ -0,0 +1,30 @@
+<?php  
+
+
+function get_all_categories($con){
+   $sql  = "SELECT * FROM categories";
+   $stmt = $con->prepare($sql);
+   $stmt->execute();
+
+   if ($stmt->rowCount() > 0) {
+   	  $categories = $stmt->fetchAll();
+   }else {
+      $categories = 0;
+   }
+
+   return $categories;
+}
+
+function get_category($con, $id){
+   $sql  = "SELECT * FROM categories WHERE id=?";
+   $stmt = $con->prepare($sql);
+   $stmt->execute([$id]);
+
+   if ($stmt->rowCount() > 0) {
+   	  $category = $stmt->fetch();
+   }else {
+      $category = 0;
+   }
+
+   return $category;
+}
