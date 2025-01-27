0ac51ae95ab9e0f365fb40aabec7742274fe46ba
ToskaK6 <toskakastrati@gmail.com>
2025-01-27 20:13:36 +0100

    Ndryshim ne php (login)

--- login.php
+++ login.php
@@ -1,3 +1,9 @@
+<?php  
+include "db_conn.php";
+
+session_start();
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,17 +14,24 @@
 </head>
 <body style="background-color: #8e4e1d;">
     <div class="login-container">
+        
+        <form action="php/auth.php" method="POST">
         <h1>LOGIN</h1>
-        <form action="#">
+        <?php
+            if (isset($_SESSION['message'])) {
+                echo "<div class='alert alert-danger'>" . $_SESSION['message'] . "</div>";
+                unset($_SESSION['message']);
+            }
+            ?>
             <label>Email address</label>
             <input type="email" id="email" name="email" placeholder="Enter your email" required>
             
             <label>Password</label>
             <input type="password" id="password" name="password" placeholder="Enter your password" required>
             <div>
-            <button type="submit" class="login-btn">Login</button>
-            <p class="signup-btn">Don't have an account? <span><a href="signup.html">SIGN UP</a></span></p>
-            <a href="index.html" class="back-link">Go Back</a>
+            <button type="submit" name="login" class="login-btn">Login</button>
+            <p class="signup-btn">Don't have an account? <span><a href="signup.php">SIGN UP</a></span></p>
+            <a href="index.php" class="back-link">Go Back</a>
         </div>
         </form>
     </div>
