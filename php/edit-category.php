35733cace4a57190142c7380e1d9cf6988ec0b7f
valzza <valzadalipi2@gmail.com>
2025-01-27 19:17:28 +0100

    Create edit-category.php

---
+++ php/edit-category.php
@@ -0,0 +1,43 @@
+<?php  
+session_start();
+
+if (isset($_SESSION['user_id']) &&
+    isset($_SESSION['user_email'])) {
+
+	include "../db_conn.php";
+
+	if (isset($_POST['category_name']) &&
+        isset($_POST['category_id'])) {
+		$name = $_POST['category_name'];
+		$id = $_POST['category_id'];
+
+		if (empty($name)) {
+			$em = "The category name is required";
+			header("Location: ../edit-category.php?error=$em&id=$id");
+            exit;
+		}else {
+			$sql  = "UPDATE categories 
+			         SET name=?
+			         WHERE id=?";
+			$stmt = $conn->prepare($sql);
+			$res  = $stmt->execute([$name, $id]);
+
+		     if ($res) {
+		     	$sm = "Successfully updated!";
+				header("Location: ../edit-category.php?success=$sm&id=$id");
+	            exit;
+		     }else{
+		     	$em = "Unknown Error Occurred!";
+				header("Location: ../edit-category.php?error=$em&id=$id");
+	            exit;
+		     }
+		}
+	}else {
+      header("Location: ../admin.php");
+      exit;
+	}
+
+}else{
+  header("Location: ../login.php");
+  exit;
+}
