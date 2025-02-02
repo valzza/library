2e0097adc7007b2eadccb04b3e354c8401e1c6c3
ToskaK6 <toskakastrati@gmail.com>
2025-02-02 22:20:21 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -37,6 +37,9 @@ if (isset($_SESSION['user_id']) &&
     <!-- bootstrap 5 Js bundle CDN-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="style.css">
+	
+	<!-- Swiper .js CSS -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css">
 </head>
 <body>
 <header>
